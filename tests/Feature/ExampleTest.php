bfe67bf88ec50d1aad620e17038ec24fa57c9dfd
hesdin <hesdin01@gmail.com>
2026-06-26 20:27:59 +0800

    test: update ExampleTest with Inertia assertions

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,7 +1,16 @@
 <?php
 
+use Inertia\Testing\AssertableInertia;
+
 test('returns a successful response', function () {
     $response = $this->get(route('home'));
 
-    $response->assertOk();
+    $response
+        ->assertOk()
+        ->assertInertia(fn (AssertableInertia $page) => $page
+            ->component('Landing')
+            ->where('hospitalName', 'Rumah Sakit Ibnu Sina YW-UMI Makassar')
+            ->has('contact')
+            ->has('testimonials', 3)
+            ->has('insights', 3));
 });
